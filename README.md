b9895ccece12ede802665e1b10bd51c6e953b147
Mauro Codella <mcodella@zendesk.com>
2016-04-29 14:23:57 +0200

    docs

--- README.md
+++ README.md
@@ -3,6 +3,14 @@ When I started looking into the Elixir API to create a supervision tree, I ended
 
 Then, I stumbled upon [Understanding Exit Signals in Erlang/Elixir](http://crypt.codemancers.com/posts/2016-01-24-understanding-exit-signals-in-erlang-slash-elixir/), published by Emil Soman on February 29th, 2016.
 
+After reading this nicely made article, I've certainly got a better understanding of the exit signals mechanism, and wether they should be handled, whenever possible.
+
+Nonetheless, I felt the need of writing some code to observe how processes react to signals, at hand. Also, I started to wonder how processes linked to other receiving signals react - so I decided to write down some tests myself, to implements those scenarios.
+
+Of course, when it boils down to talk about exit signals, what we are really talking about are Erlang signals, but the tests have an Elixir twist.
+
+I wrote the tests using Elixir, using ExUnit as test framework.
+
 ## What happens to a not-trapping process linked to a process...
 ### ...  receiving `:normal` signal?
 
--- lib/puppet.ex
+++ lib/puppet.ex
@@ -49,7 +49,7 @@ defmodule Puppet do
   end
 
   #
-  # Puppet process related logic
+  # Puppet message listener
   #
   defmodule Listener do
     def run(trap_exit, spawner) do
@@ -64,33 +64,26 @@ defmodule Puppet do
         {:ping, sender} ->
           send(sender, {:pong, self})
           listen(state)
-
-        # COMMAND: requests the process to establish a link
-        #   with another process
+        # COMMAND: requests the process to establish a link with another process
         {:link, sender, pid} ->
           Process.link(pid)
           send sender, {:linked, self}
           listen(state)
-
         # COMMAND: request the last unhandled received message
         {:fetch_latest_unhandled_message, sender} ->
           send sender, {:fetched, self, state.latest_unhandled_message}
           listen(state)
-
         # COMMAND: requests a signal to be sent to self
         {:self_exit, signal} ->
           Process.exit(self, signal)
           listen(state)
-
         # COMMAND: requests an exception to be raised
         {:raise, message} ->
           raise message
-
         # COMMAND: requests a normal termination
         :terminate ->
           nil
-
-        # stores the last unhandled message
+        # stores the latest unhandled message
         message ->
           listen(%{state | latest_unhandled_message: message})
       end
