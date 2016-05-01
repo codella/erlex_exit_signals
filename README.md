ad7bd52896bb9ad94af104e5aa34e9b458abbb50
Mauro Codella <mauro.codella@gmail.com>
2016-05-01 11:57:35 +0200

    refactoring

--- README.md
+++ README.md
@@ -3,19 +3,10 @@ When I started looking into the Elixir API to create a supervision tree, I ended
 
 Then, I stumbled upon [Understanding Exit Signals in Erlang/Elixir](http://crypt.codemancers.com/posts/2016-01-24-understanding-exit-signals-in-erlang-slash-elixir/), published by Emil Soman on February 29th, 2016.
 
-After reading this nicely made article, I've certainly got a better understanding of the exit signals mechanism, and wether they should be handled, whenever possible.
+After reading this nicely made article, I've certainly got a better understanding of the exit signals mechanism and wether they should be handled, whenever possible.
 
-Nonetheless, I felt the need of writing some code to observe how processes react to signals, at hand. Also, I started to wonder how processes linked to other receiving signals react - so I decided to write down some tests myself, to implements those scenarios.
+Since I tend to dive into some code to verify what I read around, I wrote some tests to closely observe how processes react to exit signals. Also, I started to wonder how processes linked to other receiving signals react - so I decided to write down some more tests to implements those scenarios.
 
-Of course, when it boils down to talk about exit signals, what we are really talking about are Erlang signals, but the tests have an Elixir twist.
+Of course, when it boils down to talk about exit signals, what we are really talking about are Erlang signals. Nonetheless, I decided to give it an Elixir twist, using ExUnit as test framework.
 
-I wrote the tests using Elixir, using ExUnit as test framework.
-
-## What happens to a not-trapping process linked to a process...
-### ...  receiving `:normal` signal?
-
-```
-[P (lives)] ⇆ [L]
- ⇧
-Process.exit(P, :normal)
-```
+Hope you'll enjoy it!
--- lib/puppet.ex
+++ lib/puppet.ex
@@ -1,7 +1,7 @@
 defmodule Puppet do
   def start(options) do
     {:ok, trap_exit} = Keyword.fetch(options, :trap_exit)
-    puppet = spawn(Puppet.Listener, :run, [trap_exit, self])
+    puppet = spawn(Puppet.Listener, :run, [trap_exit, self()])
 
     receive do
       {:started, ^puppet} -> puppet
@@ -9,7 +9,7 @@ defmodule Puppet do
   end
 
   def link(puppet, pid) do
-    send(puppet, {:link, self, pid})
+    send(puppet, {:link, self(), pid})
 
     receive do
       {:linked, ^puppet} -> puppet
@@ -17,7 +17,7 @@ defmodule Puppet do
   end
 
   def latest_unhandled_message(puppet) do
-    send(puppet, {:fetch_latest_unhandled_message, self})
+    send(puppet, {:fetch_latest_unhandled_message, self()})
 
     receive do
       {:fetched, ^puppet, message} -> message
@@ -41,7 +41,7 @@ defmodule Puppet do
   end
 
   def alive?(puppet) do
-    send(puppet, {:ping, self})
+    send(puppet, {:ping, self()})
 
     receive do
       {:pong, ^puppet} -> nil
@@ -50,51 +50,5 @@ defmodule Puppet do
     end
 
     Process.alive?(puppet)
-  end
-
-  #
-  # Puppet message listener
-  #
-  defmodule Listener do
-    def run(trap_exit, spawner) do
-      Process.flag(:trap_exit, trap_exit)
-      send(spawner, {:started, self})
-      listen(%{spawner: spawner, latest_unhandled_message: nil})
-    end
-
-    defp listen(state) do
-      receive do
-        # COMMAND: requests a :pong message to be sent back
-        {:ping, sender} ->
-          send(sender, {:pong, self})
-          listen(state)
-        # COMMAND: requests the process to establish a link with another process
-        {:link, sender, pid} ->
-          Process.link(pid)
-          send sender, {:linked, self}
-          listen(state)
-        # COMMAND: request the last unhandled received message
-        {:fetch_latest_unhandled_message, sender} ->
-          send sender, {:fetched, self, state.latest_unhandled_message}
-          listen(state)
-        # COMMAND: requests a signal to be sent to self via Process.exit/2
-        {:self_process_exit, signal} ->
-          Process.exit(self, signal)
-          listen(state)
-        # COMMAND: requests a signal to be sent to self via Kernel.exit/1
-        {:kernel_exit, signal} ->
-          exit(signal)
-          listen(state)
-        # COMMAND: requests an exception to be raised
-        {:raise, message} ->
-          raise message
-        # COMMAND: requests a normal termination
-        :terminate ->
-          nil
-        # stores the latest unhandled message
-        message ->
-          listen(%{state | latest_unhandled_message: message})
-      end
-    end
   end
 end
